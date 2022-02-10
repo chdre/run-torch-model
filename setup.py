19327fc05c2148c8b6ada2a1d198a594009f8818
chrdre <christerdr@outlook.com>
2022-02-10 14:35:14 +0100

    comma

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author='Christer Dreierstad',
     author_email='christerdr@outlook.com',
     description='Package for training and evaluating neural network models made using pytorch',
-    long_description=long_description
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/chdre/run-torch-model',
     packages=setuptools.find_packages(),
