Cleaned code, removed half precision, bugfix, corrected scaling of data

run_torch_model/tools.py
@@ -8,15 +8,11 @@ import warnings
 
 def create_dataloader(features, targets, batch_size, train_size=0.8,
                       test_size=0.2, validation_size=0, seed=42,
-                      scale_data=False, half_precision=False, **kwargs):
+                      scale_data=False, **kwargs):
     """Creates a Pytorch compatible dataset of type dataloader. Data is split
     in two or three batches consisting depending on the sizes of train, test
     and validation split.
 
-
-    TO-DO:
-        - Implement and test autocast for Automatic Mixed Precision (AMP).
-        Only to be active with compatible GPUs.
 
     :param features: Input (or regressor) or location of file containing the
                      features for the ML model. If location is supplied the file
@@ -42,9 +38,6 @@ def create_dataloader(features, targets, batch_size, train_size=0.8,
     :param scale_data: Whether to scale the data by sklearn StandardScaler.
                        Follows (x - mean(x)) / std(x). Defaults to False.
     :type scale_data: bool
-    :param half_precision: Whether to use half precision, i.e. float16, for
-                           torch.Tensors. Defaults to False.
-    :type half_precision: bool
     :returns data: Tuple of train, test (and validation) dataloaders
     :rtype: tuple of type torch.dataloader
     """
@@ -78,10 +71,6 @@ def create_dataloader(features, targets, batch_size, train_size=0.8,
         x = torch.Tensor(features)
         y = torch.Tensor(targets)
 
-        if half_precision:
-            x = x.type(torch.float16)
-            y = y.type(torch.float16)
-
         dataset = data.TensorDataset(x, y)
 
         train, test, validation = data.random_split(
@@ -98,22 +87,28 @@ def create_dataloader(features, targets, batch_size, train_size=0.8,
             xtest, xval, ytest, yval = train_test_split(
                 xtest, ytest, test_size=validation_size)
 
-            yval = scaler.fit_transform(yval)
-            validation = data.TensorDataset(
-                torch.Tensor(xval), torch.Tensor(yval))
-
         ytrain = scaler.fit_transform(ytrain)
-        ytest = scaler.fit_transform(ytest)
+        ytest = scaler.transform(ytest)
+
+        try:
+            yval = scaler.transform(yval)
+        except:
+            pass
 
         train = data.TensorDataset(torch.Tensor(xtrain), torch.Tensor(ytrain))
         test = data.TensorDataset(torch.Tensor(xtest), torch.Tensor(ytest))
 
     dataloader_train = data.DataLoader(train, batch_size=batch_size, **kwargs)
+
     # Settings shuffle to False for test (and validation)
     if kwargs['shuffle']:
         kwargs['shuffle'] = False
     dataloader_test = data.DataLoader(test, batch_size=batch_size, **kwargs)
-    if validation_size != 0:
+
+    if validation_size > 0:
+        if scale_data:
+            validation = data.TensorDataset(
+                torch.Tensor(xval), torch.Tensor(yval))
         dataloader_valid = data.DataLoader(
             validation, batch_size=batch_size, **kwargs)
 